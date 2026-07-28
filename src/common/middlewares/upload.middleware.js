d4ce409929b87d6ec0ba2dbbb8acc36345092f20
sai kumar <kumarchembeti26@gmail.com>
2026-07-28 14:56:55 +0530

    added file validation and cleanup

--- src/common/middlewares/upload.middleware.js
+++ src/common/middlewares/upload.middleware.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { pipeline } = require('stream/promises');
 const crypto = require('crypto');
 
-const uploadMiddleware = ({ destination = "files" } = {}) => {
+const uploadMiddleware = ({ destination = "files", maxFileSize = 500 * 1024 * 1024 } = {}) => {
 
     return (req, res, next) => {
 
@@ -18,13 +18,19 @@ const uploadMiddleware = ({ destination = "files" } = {}) => {
         }
 
         const busboy = Busboy({
-            headers: req.headers
+            headers: req.headers,
+            limits: {
+                fileSize: maxFileSize,
+            }
         });
 
         const fields = {};
         const files = [];
 
         const uploadPromises = [];
+        const actualPaths = [];
+
+        let hasFileTooLarge = false;
 
         busboy.on("field", (fieldName, value) => {
             fields[fieldName] = value;
@@ -41,6 +47,12 @@ const uploadMiddleware = ({ destination = "files" } = {}) => {
             const actualPath = path.join(uploadDir, uniqueName);
             const storedPath = `${destination}/${uniqueName}`;
 
+            actualPaths.push(actualPath);
+
+            file.on("limit", () => {
+                hasFileTooLarge = true;
+            });
+
             const writeStream = fs.createWriteStream(actualPath);
 
             files.push({ fieldName, filename: uniqueName, mimeType: info.mimeType, path: storedPath });
@@ -53,19 +65,42 @@ const uploadMiddleware = ({ destination = "files" } = {}) => {
         busboy.on("finish", async () => {
             try {
                 await Promise.all(uploadPromises);
+
+                if (files.length === 0) {
+                    return res.status(400).json({
+                        success: false,
+                        message: "No files or fields were uploaded.",
+                    });
+                }
+
+                if (hasFileTooLarge) {
+                    await cleanupFiles(actualPaths);
+                    return res.status(413).json({
+                        success: false,
+                        message: "One or more files exceed the maximum allowed size.",
+                    });
+                }
                 req.body = fields;
                 req.files = files;
                 next();
             } catch (error) {
+                await cleanupFiles(actualPaths);
                 next(error);
             }
         });
 
-        busboy.on("error", next);
+        busboy.on("error", async (error) => {
+            await cleanupFiles(actualPaths);
+            next(error);
+        });
 
         req.pipe(busboy);
 
     }
+}
+
+const cleanupFiles = async (filepaths) => {
+    await Promise.all(filepaths.map((path) => fs.promises.rm(path, { force: true })));
 }
 
 module.exports = uploadMiddleware;
